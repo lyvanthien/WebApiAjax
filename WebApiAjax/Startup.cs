1f485857adc6f4374eb9e718265f79f4a99bc111
lyvanthien <lyvanthien@LYVANTHIEN>
2016-10-10 10:02:33 +0700

    Add project files.

---
+++ WebApiAjax/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApiAjax.Startup))]
+namespace WebApiAjax
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
